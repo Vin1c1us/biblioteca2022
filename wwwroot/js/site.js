de26918ec9b672b2b8ccf605fd86c70862b71e5e
Vin1c1us <vinicius21423@gmail.com>
2022-02-21 13:46:40 -0300

    Paginação na listagem de emprestimo e de livros e marcação de atrasado e devolvido implementado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,47 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function Paginacao(page){
+    console.log("Page " + page);
 
-// Write your JavaScript code.
+    var currentPage = document.getElementById("currentPage");
+    console.log("CurrentPage " + currentPage.value);
+    var maxPage = document.getElementById("maxPage");
+    console.log("MaxPage " + maxPage.value);
+
+    var tabelaAtiva = document.getElementById("table-" + currentPage.value);
+    var ativarTabela = document.getElementById("table-" + page);
+    var pageItem = document.getElementsByClassName("page-item");
+
+    if(page > 0){
+        document.getElementById("prev").classList.remove("disabled")
+    }
+    else{
+        document.getElementById("prev").classList.add("disabled")
+    }
+
+    if(page < parseInt(maxPage.value) - 1){
+        document.getElementById("next").classList.remove("disabled")
+    }
+    else{
+        document.getElementById("next").classList.add("disabled")
+    }
+
+    tabelaAtiva.style.display = "none";
+
+    ativarTabela.style.display = "";
+
+    
+    if(pageItem !== null){
+        pageItem[page + 1].classList.add("active");
+        pageItem[parseInt(currentPage.value) + 1].classList.remove("active");
+    }
+    
+    currentPage.value = page;
+
+    console.log("CurrentPage " + currentPage.value);
+
+}
+
+function MudarPagina(mudar){
+    var currentPage = parseInt(document.getElementById("currentPage").value);
+
+    Paginacao(currentPage + mudar);
+}
